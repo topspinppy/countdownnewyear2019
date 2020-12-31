7b46afb88e90394a601a2c5bc6520d15893912c9
Paranat (Top) Klinhom <paranat_kli@true-e-logistics.com>
2020-12-31 22:51:06 +0700

    update

--- pages/utils/CountdownNewYear.js
+++ pages/utils/CountdownNewYear.js
@@ -6,16 +6,17 @@ const TextStyled = styled.div`
 `
 export default function CountdownTimer(props) {
     const calculateTimeLeft = () => {
-        const difference = +new Date("2021-01-01") - +new Date();
+        let year = new Date().getFullYear();
+        const difference = +new Date(`2021-1-1`) - +new Date();
         let timeLeft = {};
-
+    
         if (difference > 0) {
-            timeLeft = {
-                วัน: Math.floor(difference / (1000 * 60 * 60 * 24)),
-                ชั่วโมง: Math.floor((difference / (1000 * 60 * 60)) % 24),
-                นาที: Math.floor((difference / 1000 / 60) % 60),
-                วินาที: Math.floor((difference / 1000) % 60)
-            };
+          timeLeft = {
+            วัน: Math.floor(difference / (1000 * 60 * 60 * 24)),
+            ชั่วโมง: Math.floor((difference / (1000 * 60 * 60)) % 24),
+            นาที: Math.floor((difference / 1000 / 60) % 60),
+            วินาที: Math.floor((difference / 1000) % 60),
+          };
         }
 
         return timeLeft;
@@ -23,24 +24,25 @@ export default function CountdownTimer(props) {
 
     const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
+  
     useEffect(() => {
-        setTimeout(() => {
-            setTimeLeft(calculateTimeLeft());
-        }, 1000);
+      setTimeout(() => {
+        setTimeLeft(calculateTimeLeft());
+      }, 1000);
     });
-
+  
     const timerComponents = [];
-
-    Object.keys(timeLeft).forEach(interval => {
-        if (!timeLeft[interval]) {
-            return;
-        }
-
-        timerComponents.push(
-            <span>
-                {timeLeft[interval]} {interval}{" "}
-            </span>
-        );
+  
+    Object.keys(timeLeft).forEach((interval) => {
+      if (!timeLeft[interval]) {
+        return;
+      }
+  
+      timerComponents.push(
+        <span>
+          {timeLeft[interval]} {interval}{" "}
+        </span>
+      );
     });
     return (
         <>
